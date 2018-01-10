292e3638a9ff408a4f7fbba628537809c29b9574
Muneeb Aadil <imuneebaadil@gmail.com>
2018-01-10 11:53:13 +0500

    added estimateFundMat()

---
+++ sfmUpenn.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import scipy.io as io 
+
+def EstimateFundamentalMatrix(x1,x2): 
+    A = np.zeros((8,9))
+    for i in xrange(8):
+        rowA = np.array([x1[i,0]*x2[i,0], x1[i,0]*x2[i,1], x1[i,0],
+              x1[i,1]*x2[i,0], x1[i,1]*x2[i,1], x1[i,1],
+              x2[i,0], x2[i,1], 1])
+        A[i,:] = rowA
+
+    u,s,v = np.linalg.svd(A)
+    F = v[-1,:].reshape((3,3),order='F')
+
+    u,s,v = np.linalg.svd(F)
+    F = u.dot(np.diag(s).dot(v))
+
+    F = F / np.linalg.norm(F,'fro')
+    
+    return F 
+
+def RunSFM(filename): 
+    variables = io.loadmat(filename)
+    pass 
+
+if __name__=='__main__':
+    filename = 'variables.mat'
+    RunSFM(filename)
