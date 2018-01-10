9aee4d71a8c1e728b3b3b4d9e0558df8295e62d9
Muneeb Aadil <imuneebaadil@gmail.com>
2018-01-10 18:22:01 +0500

    added linearTriangulate() method

--- sfmUpenn.py
+++ sfmUpenn.py
@@ -31,6 +31,30 @@ def EstimateEssentialMatrix(K,F):
     
     return E
 
+def Vec2Skew(vec): 
+    return np.array([[0,-vec[2],vec[1]],[vec[2],0,-vec[0]],[-vec[1],vec[0],0]])
+
+def linearTriangulate(K, C1, R1, C2, R2, x1, x2): 
+    P1 = K.dot(np.hstack((R1,C1)))
+    P2 = K.dot(np.hstack((R2,C2)))
+    
+    X = np.zeros((x1.shape[0],3))
+    x1h = np.hstack((x1, np.ones((x1.shape[0],1))))
+    x2h = np.hstack((x2, np.ones((x2.shape[0],1))))
+    
+    for i in xrange(x1h.shape[0]): 
+        x1hcross = Vec2Skew(x1h[i,:])
+        x2hcross = Vec2Skew(x2h[i,:])
+
+        A = np.vstack((x1hcross.dot(P1),x2hcross.dot(P2)))
+
+        u,s,v = np.linalg.svd(A)
+
+        ans = v[-1,:3] / v[-1,-1]
+        X[i,:] = ans 
+        
+    return X
+
 def RunSFM(filename): 
     variables = io.loadmat(filename)
     pass 
