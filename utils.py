11085738aa792b878cba9b3a51af1a54d93960d0
Muneeb Aadil <imuneebaadil@gmail.com>
2018-01-27 21:07:31 +0500

    fixed PlotCamera()

--- utils.py
+++ utils.py
@@ -103,14 +103,19 @@ def DrawMatchesCustom(img1,img2,kp1,kp2,F,drawOnly=None):
 
 def PlotCamera(R,t,ax,scale=.5,depth=.5):
     C = -t #camera center (in world coordinate system)
+
+    #Generating camera coordinate axes
+    axes = np.zeros((3,6))
+    axes[0,1], axes[1,3],axes[2,5] = 1,1,1
     
-    #plotting x-axis of camera coordinate system (red line)
-    ax.plot3D(xs=[C[0],R[0,0]+C[0]],ys=[C[1],R[1,0]+C[1]],zs=[C[2],R[2,0]+C[2]],c='r')
-    #plotting y-axis of camera coordinate system (green line)
-    ax.plot3D(xs=[C[0],R[0,1]+C[0]],ys=[C[1],R[1,1]+C[1]],zs=[C[2],R[2,1]+C[2]],c='g')
-    #plotting z-axis of camera coordinate system (blue line)
-    ax.plot3D(xs=[C[0],R[0,2]+C[0]],ys=[C[1],R[1,2]+C[1]],zs=[C[2],R[2,2]+C[2]],c='b')
-    
+    #Transforming to world coordinate system 
+    axes = R.T.dot(axes)+C[:,np.newaxis]
+
+    #Plotting axes
+    ax.plot3D(xs=axes[0,:2],ys=axes[1,:2],zs=axes[2,:2],c='r')
+    ax.plot3D(xs=axes[0,2:4],ys=axes[1,2:4],zs=axes[2,2:4],c='g')
+    ax.plot3D(xs=axes[0,4:],ys=axes[1,4:],zs=axes[2,4:],c='b')
+
     #generating 5 corners of camera polygon 
     pt1 = np.array([[0,0,0]]).T #camera centre
     pt2 = np.array([[scale,-scale,depth]]).T #upper right 
@@ -120,7 +125,7 @@ def PlotCamera(R,t,ax,scale=.5,depth=.5):
     pts = np.concatenate((pt1,pt2,pt3,pt4,pt5),axis=-1)
     
     #Transforming to world-coordinate system
-    pts = R.dot(pts)+C[:,np.newaxis]
+    pts = R.T.dot(pts)+C[:,np.newaxis]
     ax.scatter3D(xs=pts[0,:],ys=pts[1,:],zs=pts[2,:],c='k')
     
     #Generating a list of vertices to be connected in polygon
