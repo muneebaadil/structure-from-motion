7690a87b631fa2bb48b65697d5e6597829804e23
Muneeb Aadil <imuneebaadil@gmail.com>
2018-02-09 15:27:19 +0500

    added a couple of functions to utils (from tutorial folder)

--- opencv/utils.py
+++ opencv/utils.py
@@ -102,3 +102,50 @@ def GetAlignedMatches(kp1,desc1,kp2,desc2,matches):
     img2pts = np.array([kp.pt for kp in kp2_])
 
     return img1pts,img2pts
+
+def GetTriangulatedPts(img1pts,img2pts,K,R,t): 
+    """Triangulates a pair of 2D points into corresponding 3D points
+    
+    Args: 
+    img1pts: (n,2) array of 2D keypoints
+    img2pts: (n,2) array of corresponding 2D keypoints
+    K: (3,3) Camera calibration matrix 
+    R: (3,3) Camera rotation matrix
+    t: (3,1) Camera translation matrix
+    
+    Returns: 
+    out: (n,3) 3D coordinates of n points"""
+    img1ptsHom = cv2.convertPointsToHomogeneous(img1pts)[:,0,:]
+    img2ptsHom = cv2.convertPointsToHomogeneous(img2pts)[:,0,:]
+
+    img1ptsNorm = (np.linalg.inv(K).dot(img1ptsHom.T)).T
+    img2ptsNorm = (np.linalg.inv(K).dot(img2ptsHom.T)).T
+
+    img1ptsNorm = cv2.convertPointsFromHomogeneous(img1ptsNorm)[:,0,:]
+    img2ptsNorm = cv2.convertPointsFromHomogeneous(img2ptsNorm)[:,0,:]
+
+    pts4d = cv2.triangulatePoints(np.eye(3,4),np.hstack((R,t)),img1ptsNorm.T,img2ptsNorm.T)
+    pts3d = cv2.convertPointsFromHomogeneous(pts4d.T)[:,0,:]
+
+    return pts3d
+
+def pts2ply(pts,filename='out.ply'): 
+    """Saves an ndarray of 3D coordinates (in meshlab format)"""
+
+    with open(filename,'w') as f: 
+        f.write('ply\n')
+        f.write('format ascii 1.0\n')
+        f.write('element vertex {}\n'.format(pts.shape[0]))
+        
+        f.write('property float x\n')
+        f.write('property float y\n')
+        f.write('property float z\n')
+        
+        f.write('property uchar red\n')
+        f.write('property uchar green\n')
+        f.write('property uchar blue\n')
+        
+        f.write('end_header\n')
+        
+        for pt in pts: 
+            f.write('{} {} {} 255 255 255\n'.format(pt[0],pt[1],pt[2]))
