1d526393bd707b10ec3050eb2ef081d05f16c201
Muneeb Aadil <imuneebaadil@gmail.com>
2018-05-08 10:48:37 +0500

    featmatching done, ignoring txt files

--- script/featmatch.sh
+++ script/featmatch.sh
@@ -6,12 +6,12 @@ python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/fo
 
 #ENTRY P10 DATASET
 #SURF
-python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/entry-P10/ --features SURF
+python featmatch.py --data_dir ../data/entry-P10/images/ --out_dir ../data/entry-P10/ --features SURF
 #SIFT
 python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/entry-P10/ --features SIFT
 
 #HERZ JESUS P8 DATASET
 #SURF
-python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/Herz-Jesus-P8/ --features SURF
+python featmatch.py --data_dir ../data/Herz-Jesus-P8/images/ --out_dir ../data/Herz-Jesus-P8/ --features SURF
 #SIFT
 python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/Herz-Jesus-P8/ --features SIFT
