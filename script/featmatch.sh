29e486d506f56912149f2decff42f298a3e2c287
Muneeb Aadil <imuneebaadil@gmail.com>
2018-05-08 07:45:31 +0500

    added bash scripts for experiments generation

---
+++ script/featmatch.sh
@@ -0,0 +1,17 @@
+#FOUNTAIN P11 DATASET
+#SURF
+python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/fountain-P11/ --features SURF
+#SIFT
+python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/fountain-P11/ --features SIFT
+
+#ENTRY P10 DATASET
+#SURF
+python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/entry-P10/ --features SURF
+#SIFT
+python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/entry-P10/ --features SIFT
+
+#HERZ JESUS P8 DATASET
+#SURF
+python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/Herz-Jesus-P8/ --features SURF
+#SIFT
+python featmatch.py --data_dir ../data/fountain-P11/images/ --out_dir ../data/Herz-Jesus-P8/ --features SIFT
---
+++ script/sfm.sh
@@ -0,0 +1,38 @@
+#FOUNTAIN P11 DATASET
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_DLS --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_EPNP --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_P3P --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_ITERATIVE --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_UPNP --pnp_prob 0.99 --plot_error False
+
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_DLS --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_EPNP --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_P3P --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_ITERATIVE --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset fountain-P11 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_UPNP --pnp_prob 0.99 --plot_error False
+
+#HERZ JESUS P8 DATASET
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_DLS --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_EPNP --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_P3P --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_ITERATIVE --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_UPNP --pnp_prob 0.99 --plot_error False
+
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_DLS --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_EPNP --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_P3P --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_ITERATIVE --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset Herz-Jesus-P8 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_UPNP --pnp_prob 0.99 --plot_error False
+
+#ENTRY P10 DATASET
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_DLS --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_EPNP --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_P3P --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_ITERATIVE --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_RANSAC --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_UPNP --pnp_prob 0.99 --plot_error False
+
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_DLS --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_EPNP --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_P3P --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_ITERATIVE --pnp_prob 0.99 --plot_error False
+python sfm.py --dataset entry-P10 --features SURF --fund_method FM_LMEDS --outlier_thres 0.9 --fund_prob 0.99 --pnp_method SOLVEPNP_UPNP --pnp_prob 0.99 --plot_error False
